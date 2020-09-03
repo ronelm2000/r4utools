7e54bbf4cf0d1b8be893c3c040942768a6d601c0
ronelm2000 <ronelm2000@gmail.com>
2020-09-03 09:39:34 +0800

    Re-imports ActivityLog to fix GU serial issues.

---
+++ Montage.RebirthForYou.Tools.CLI/Migrations/20200903013533_RedoGochiUsa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Montage.RebirthForYou.Tools.CLI.Migrations
+{
+    public partial class RedoGochiUsa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MigrationLog",
+                keyColumn: "LogID",
+                keyValue: 1,
+                columns: new[] { "DateAdded", "IsDone" },
+                values: new object[] { new DateTime(2020, 9, 2, 0, 0, 23, 534, DateTimeKind.Local).AddTicks(9446), false });
+
+            migrationBuilder.UpdateData(
+                table: "MigrationLog",
+                keyColumn: "LogID",
+                keyValue: 2,
+                columns: new[] { "DateAdded", "IsDone" },
+                values: new object[] { new DateTime(2020, 9, 2, 0, 0, 23, 534, DateTimeKind.Local).AddTicks(9446), false });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MigrationLog",
+                keyColumn: "LogID",
+                keyValue: 1,
+                columns: new[] { "DateAdded", "IsDone" },
+                values: new object[] { new DateTime(2020, 8, 13, 17, 7, 23, 534, DateTimeKind.Local).AddTicks(9446), false });
+
+            migrationBuilder.UpdateData(
+                table: "MigrationLog",
+                keyColumn: "LogID",
+                keyValue: 2,
+                columns: new[] { "DateAdded", "IsDone" },
+                values: new object[] { new DateTime(2020, 8, 13, 17, 7, 23, 536, DateTimeKind.Local).AddTicks(1375), false });
+        }
+    }
+}
