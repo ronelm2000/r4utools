4fd08e23e7f6bb78333cd12cedf7a9c5aa74938c
ronelm2000 <ronelm2000@gmail.com>
2020-09-04 18:21:45 +0800

    Adds an official website-based post-processor, which should grab the website's image url and japanese-related text.

---
+++ Montage.RebirthForYou.Tools.CLI/Impls/PostProcessors/RebirthWebsitePostProcessor.cs
@@ -0,0 +1,81 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using Montage.RebirthForYou.Tools.CLI.API;
+using Montage.RebirthForYou.Tools.CLI.Entities;
+using Montage.RebirthForYou.Tools.CLI.Utilities;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Montage.RebirthForYou.Tools.CLI.Impls.PostProcessors
+{
+    public class RebirthWebsitePostProcessor : ICardPostProcessor
+    {
+        private readonly ILogger Log = Serilog.Log.ForContext<RebirthWebsitePostProcessor>();
+        private readonly string rebirthURLPrefix = "https://rebirth-fy.com/cardlist/?cardno=";
+        private readonly Regex effectMatcher = new Regex(@"(【(スパーク|のびしろ|ブロッカー|キャンセル|起|永|自)([^】]*)】：?)(.*)((\n[^【](.*))*)");
+
+        public int Priority => 1;
+
+        public bool IsCompatible(List<R4UCard> cards)
+        {
+            if (cards.First().Language != CardLanguage.Japanese)
+                return false;
+            var list = cards.Select(c => c.ReleaseID).Distinct().ToList();
+            if (list.Count > 1)
+            {
+                Log.Warning("Image Post-Processor is disabled for sets with multiple Release IDs; please add those images manually when prompted.");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        public async IAsyncEnumerable<R4UCard> Process(IAsyncEnumerable<R4UCard> originalCards)
+        {
+            Log.Information("Starting...");
+            await foreach (var card in originalCards)
+            {
+                yield return await Process(card);
+            }
+        }
+
+        private async Task<R4UCard> Process(R4UCard card)
+        {
+            if (!HasMissingInformation(card)) return card;
+            var updatedCard = card.Clone();
+            var url = $"{rebirthURLPrefix}{updatedCard.Serial.Replace("+", "＋")}";
+            Log.Debug("Opening Link: {url}", url);
+            var document = await url.WithHTMLHeaders().GetHTMLAsync();
+            var flavorJPText = document.QuerySelector(".cardlist-flavor").GetInnerText();
+            var rulesTextJPText = document.QuerySelector(".cardlist-free").GetInnerText().Trim();
+            var imageLink = document.QuerySelector(".cardlist-img").FindChild<IHtmlImageElement>().Source;
+            var rulesTextEnumerable = effectMatcher.Matches(rulesTextJPText);
+            Log.Information("Flavor JP: {jp}", flavorJPText);
+            Log.Information("Rules Text JP: {jp}", rulesTextJPText);
+            if (!String.IsNullOrWhiteSpace(flavorJPText))
+            {
+                card.Flavor ??= new MultiLanguageString();
+                card.Flavor.JP = flavorJPText;
+            }
+            card.Effect = rulesTextEnumerable.Select((m, i) =>
+            {
+                var result = card.Effect[i].Clone();
+                result.JP = m.Value;
+                return result;
+            }).ToArray();
+            card.Images.Add(new Uri(imageLink));
+            Log.Information("After editing: {@card}", card);
+            return updatedCard;
+        }
+
+        private bool HasMissingInformation(R4UCard card)
+        {
+            return (card?.Effect?.Any(mls => mls.JP == null) ?? false) || (card?.Name.JP == null) || (card?.Traits?.Any(mls => mls.JP == null) ?? false);
+        }
+    }
+}
