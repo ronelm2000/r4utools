768d94479f483513ff9756964cea97bb6a6ebc25
ronelm2000 <ronelm2000@gmail.com>
2020-09-25 20:16:50 +0800

    Fixes crash issue if Fandom card pages do not have an official Set page.

--- Montage.RebirthForYou.Tools.CLI/Impls/Parsers/Cards/FandomWikiSetParser.cs
+++ Montage.RebirthForYou.Tools.CLI/Impls/Parsers/Cards/FandomWikiSetParser.cs
@@ -26,7 +26,7 @@ namespace Montage.RebirthForYou.Tools.CLI.Impls.Parsers.Cards
     {
         private Regex fandomMatcher = new Regex(@"(.*)://rebirth-for-you\.fandom\.com/wiki/(.*)");
         private Regex effectMatcher = new Regex(@"(\[(CONT|AUTO|ACT|Spark|Blocker|Cancel|Relaxing|Growing)([^\]]*)\])(.*)((\n[^\[](.*))*)");
-        private Regex serialMatcher = new Regex(@"- ((\w+\/\w+)-\w*\d+\w{0,4}) \((\w*\+?)\)");
+        private Regex serialMatcher = new Regex(@"(?:- )?((\w+\/\w+)-\w*\d+\w{0,4}) \((\w*\+?)\)");
         private string[] nonFoilRarities = new string[] { "RRR", "RR", "R", "U", "C", "TD", "SD", "ReR", "ReC", "P" };
         private Func<CardDatabaseContext> _database;
 
