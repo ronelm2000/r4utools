6223ed9054946184bfa3600b440c798ee13125f4
Art Man <boloks25@gmail.com>
2021-01-04 05:20:48 +0800

    fixed insert data for PR

---
+++ Montage.RebirthForYou.Tools.CLI/Migrations/20210104051243_RedoPR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Montage.RebirthForYou.Tools.CLI.Migrations
+{
+    public partial class RedoPR : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MigrationLog",
+                keyColumn: "LogID",
+                keyValue: 14,
+                columns: new[] { "DateAdded", "IsDone" },
+                values: new object[] { new DateTime(2021, 1, 2, 0, 0, 6, 0, DateTimeKind.Local).AddTicks(9446), false });
+        }
+        
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MigrationLog",
+                keyColumn: "LogID",
+                keyValue: 14,
+                columns: new[] { "DateAdded", "IsDone" },
+                values: new object[] { new DateTime(2021, 1, 2, 0, 0, 6, 0, DateTimeKind.Local).AddTicks(9446), false });
+        }
+    }
+}
