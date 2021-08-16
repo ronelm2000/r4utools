436e92bc3bb6fec691634f480bcfc815d269a589
ronelm2000 <ronelm2000@gmail.com>
2021-08-16 21:10:43 +0800

    Fixes crash in GHA.

---
+++ Montage.RebirthForYou.Tools.GUI/Dialogs/AboutDialog.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Lamar;
+using Microsoft.Extensions.DependencyInjection;
+using Montage.RebirthForYou.Tools.GUI.ModelViews;
+
+namespace Montage.RebirthForYou.Tools.GUI.Dialogs
+{
+    public class AboutDialog : Window
+    {
+        public AboutDialog()
+        {
+            this.InitializeComponent();
+            DataContext = new AboutDialogModel();
+#if DEBUG
+//            this.AttachDevTools();
+#endif
+        }
+
+        public AboutDialog (IContainer ioc) : this()
+        {
+            DataContext = ioc.GetService<AboutDialogModel>();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Montage.RebirthForYou.Tools.GUI/ModelViews/AboutDialogModel.cs
@@ -0,0 +1,40 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Montage.RebirthForYou.Tools.GUI.ModelViews
+{
+    public class AboutDialogModel : ReactiveObject
+    {
+        #region Private Fields
+        private ObservableCollection<AboutModelRow> _rows;
+        #endregion
+
+        #region Public Properties
+        public ObservableCollection<AboutModelRow> Rows
+        {
+            get => _rows;
+            set => this.RaiseAndSetIfChanged(ref _rows, value);
+        }
+        #endregion
+
+        public AboutDialogModel()
+        {
+            Rows = new ObservableCollection<AboutModelRow>(new AboutModelRow[]
+            {
+                new AboutModelRow { Header = "Sample", Value = "Example Value" },
+                new AboutModelRow { Header = "Sample", Value = "Example Value" },
+                new AboutModelRow { Header = "Sample", Value = "Example Value" }
+            });
+        }
+
+    }
+
+    public class AboutModelRow
+    {
+        public string Header;
+        public string Value;
+    }
+}
