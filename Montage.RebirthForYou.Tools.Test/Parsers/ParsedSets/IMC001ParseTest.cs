92f9eddd70f5e291efd48220ae882be59065bec3
ronelm2000 <ronelm2000@gmail.com>
2021-08-17 17:30:02 +0800

    Renamed test as this is sourced from renegades.

--- Montage.RebirthForYou.Tools.Test/Parsers/ParsedSets/IMC001ParseTest.cs
+++ Montage.RebirthForYou.Tools.Test/Parsers/ParsedSets/IMC001ParseTest.cs
@@ -12,7 +12,7 @@ namespace Montage.RebirthForYou.Tools.Test.Parsers.ParsedSets
 {
     public partial class ParsedSetsTest
     {
-        [TestMethod("IMC/001B - IM@S Cinderella Girls Booster Pack Parse Test")]
+        [TestMethod("IMC/001B - IM@S Cinderella Girls BP Parse Test (Renegades)")]
         public async Task IMC001BTest()
         {
             await new ParseVerb { URI = "https://rebirthforyourenegades.wordpress.com/2021/02/24/imc-001b-full-set-list/" }.Run(_ioc);
