0fda6f7dfc8a3e1df84bc4cb1900fa59f27727f3
ronelm2000 <ronelm2000@gmail.com>
2021-10-17 16:26:55 +0800

    Fixes missing class for CSV Partner class

---
+++ Montage.RebirthForYou.Tools.CLI/Utilities/RebirthForYou/R4URegex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Montage.RebirthForYou.Tools.CLI.Utilities.RebirthForYou
+{
+    internal static class R4URegex
+    {
+        public static readonly Regex ReleaseIDMatcher = new(@"(([A-Za-z0-9]+)(\/)([^-]+))-");
+
+    }
+}
